9c3a9f48bbce902ae21f27f4951e77451d73ee88
Ken Wood <ken.wood@ontario.ca>
2018-06-04 15:06:04 -0400

    Add project files.

---
+++ TritechDecryptor/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using TriTech.Common;
+
+namespace TritechDecryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XMLReader();
+
+        }
+
+        private void XMLReader()
+
+        {
+            //XmlSerializer xmlSerializer = new XmlSerializer(typeof(MultiEnvironmentList));
+            // StreamReader streamReader = File.OpenText(Environment.CurrentDirectory + "\\LaunchConfiguration.xml");
+
+
+            //XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "TriTech.config")); //Combines the location of App_Data and the file name
+            //XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "LaunchInstructions - Interface.xml")); //Combines the location of App_Data and the file name
+            //XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "LaunchInstructions - VisiCAD.xml")); //Combines the location of App_Data and the file name
+            //note encrypted =  XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "LaunchConfiguration.xml")); //Combines the location of App_Data and the file name
+            //XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "New_TriTech.config")); //Combines the location of App_Data and the file name
+            //XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "New_Clear_LaunchInstructions - VisiCAD.xml")); //Combines the location of App_Data and the file name
+            //XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "New_Clear_LaunchInstructions - Interface.xml")); //Combines the location of App_Data and the file name
+
+            XmlTextReader reader = new XmlTextReader(System.IO.Path.Combine(Environment.CurrentDirectory, "LaunchInstructions.xml")); //Combines the location of App_Data and the file name
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        listBox1.Items.Add("<" + reader.Name + ">");
+                        if (reader.HasAttributes)
+                        {
+                            listBox1.Items.Add("Attributes of <" + reader.Name + ">");
+
+                            {
+                                while (reader.MoveToNextAttribute())
+                                {
+                                    if (reader.Name == "key")
+                                        listBox1.Items.Add(String.Format(" {0}={1}", reader.Name, reader.Value));
+                                    if (reader.Name == "value")
+                                        listBox1.Items.Add(String.Format(" {0}={1}", reader.Name, Cryptography.Decrypt(reader.Value, "Europa")));
+                                }
+                            }
+
+                            // Move the reader back to the element node.
+                            reader.MoveToElement();
+                        }
+                        break;
+                    case XmlNodeType.Text:
+                        listBox1.Items.Add(Cryptography.Decrypt(reader.Value, "Europa"));
+                        break;
+                    case XmlNodeType.Attribute:
+                        listBox1.Items.Add(Cryptography.Decrypt(reader.Value, "Europa"));
+                        break;
+                    case XmlNodeType.EndElement:
+                        listBox1.Items.Add("");
+                        break;
+                }
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //DecryptTriTechConfig();
+
+            DecryptLaunchConfigs("LaunchInstructions - Interface.xml");
+
+            DecryptLaunchConfigs("LaunchInstructions - VisiCAD.xml");
+
+
+
+        }
+        private void NewTriTechConfig()
+        {
+
+            string configFile = System.IO.Path.Combine(Environment.CurrentDirectory, "Clear_TriTech.config");
+            XDocument xmlDoc = XDocument.Load(configFile);
+
+            var nodes = from item in xmlDoc.DescendantNodes() select item;
+            foreach (XNode node in nodes)
+
+            {
+                if (node is XElement)
+                {
+                    //listBox1.Items.Add((node as XElement).Name);
+                    if ((node as XElement).Name == "add")
+                    {
+
+                        string value = (node as XElement).Attribute("value").Value;
+                        (node as XElement).SetAttributeValue("value", Cryptography.Encrypt(value, "Europa"));
+                    }
+                }
+
+
+
+            }
+            xmlDoc.Save("New_TriTech.config");
+        }
+        private void  DecryptTriTechConfig()
+        {
+
+            string configFile = System.IO.Path.Combine(Environment.CurrentDirectory, "TriTech.config");
+            XDocument xmlDoc = XDocument.Load(configFile);
+
+            var nodes = from item in xmlDoc.DescendantNodes() select item;
+            foreach (XNode node in nodes)
+
+            {
+                if (node is XElement)
+                {
+                    //listBox1.Items.Add((node as XElement).Name);
+                    if ((node as XElement).Name == "add")
+                    {
+                        string value = (node as XElement).Attribute("value").Value;
+
+                        (node as XElement).SetAttributeValue("value", Cryptography.Decrypt(value, "Europa"));
+                    }
+                }
+
+                //else
+                //    listBox1.Items.Add(node);
+
+            }
+            xmlDoc.Save("Clear_TriTech.config");
+        }
+
+        private void DecryptLaunchConfigs(string fileName)
+        {
+
+            string configFile = System.IO.Path.Combine(Environment.CurrentDirectory, fileName);
+            XDocument xmlDoc = XDocument.Load(configFile);
+
+            var nodes = from item in xmlDoc.DescendantNodes() select item;
+            foreach (XNode node in nodes)
+
+            {
+                if (node is XElement)
+                {
+                    XElement element = (XElement)node;
+                    if (!element.HasElements && !element.IsEmpty)
+                    {
+                        string value = Cryptography.Decrypt(element.Value, "Europa") ;
+                        element.SetValue(value);
+
+                       //
+                    }
+
+
+                }
+
+
+
+            }
+            xmlDoc.Save("Clear_" + fileName);
+        }
+        private void EncryptCleanLaunchConfigs(string fileName)
+        {
+
+            string configFile = System.IO.Path.Combine(Environment.CurrentDirectory, fileName);
+            XDocument xmlDoc = XDocument.Load(configFile);
+
+            var nodes = from item in xmlDoc.DescendantNodes() select item;
+            foreach (XNode node in nodes)
+
+            {
+                if (node is XElement)
+                {
+                    XElement element = (XElement)node;
+                    if (!element.HasElements && !element.IsEmpty)
+                    {
+                        string value = Cryptography.Encrypt(element.Value, "Europa");
+                        element.SetValue(value);
+                    }
+
+
+                }
+
+
+
+            }
+            xmlDoc.Save("New_" + fileName);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            NewTriTechConfig();
+
+
+            EncryptCleanLaunchConfigs("Clear_LaunchInstructions - Interface.xml");
+
+            EncryptCleanLaunchConfigs("Clear_LaunchInstructions - VisiCAD.xml");
+        }
+    }
+}
---
+++ TritechDecryptor/TritechCrypto.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TriTech.Common
+{
+    public class Cryptography
+    {
+        private Cryptography()
+        {
+        }
+
+        public static byte[] Encrypt(byte[] clearData, byte[] Key, byte[] IV)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            Rijndael rijndael = Rijndael.Create();
+            rijndael.Key = Key;
+            rijndael.IV = IV;
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+            cryptoStream.Write(clearData, 0, clearData.Length);
+            cryptoStream.Close();
+            return memoryStream.ToArray();
+        }
+
+        public static string Encrypt(string clearText, string Password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(clearText);
+            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(Password, new byte[]
+            {
+                73,
+                118,
+                97,
+                110,
+                32,
+                77,
+                101,
+                100,
+                118,
+                101,
+                100,
+                101,
+                118
+            });
+            byte[] inArray = Cryptography.Encrypt(bytes, passwordDeriveBytes.GetBytes(32), passwordDeriveBytes.GetBytes(16));
+            return Convert.ToBase64String(inArray);
+        }
+
+        public static byte[] Encrypt(byte[] clearData, string Password)
+        {
+            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(Password, new byte[]
+            {
+                73,
+                118,
+                97,
+                110,
+                32,
+                77,
+                101,
+                100,
+                118,
+                101,
+                100,
+                101,
+                118
+            });
+            return Cryptography.Encrypt(clearData, passwordDeriveBytes.GetBytes(32), passwordDeriveBytes.GetBytes(16));
+        }
+
+        public static void Encrypt(string fileIn, string fileOut, string Password)
+        {
+            FileStream fileStream = new FileStream(fileIn, FileMode.Open, FileAccess.Read);
+            FileStream stream = new FileStream(fileOut, FileMode.OpenOrCreate, FileAccess.Write);
+            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(Password, new byte[]
+            {
+                73,
+                118,
+                97,
+                110,
+                32,
+                77,
+                101,
+                100,
+                118,
+                101,
+                100,
+                101,
+                118
+            });
+            Rijndael rijndael = Rijndael.Create();
+            rijndael.Key = passwordDeriveBytes.GetBytes(32);
+            rijndael.IV = passwordDeriveBytes.GetBytes(16);
+            CryptoStream cryptoStream = new CryptoStream(stream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+            int num = 4096;
+            byte[] buffer = new byte[num];
+            int num2;
+            do
+            {
+                num2 = fileStream.Read(buffer, 0, num);
+                cryptoStream.Write(buffer, 0, num2);
+            }
+            while (num2 != 0);
+            cryptoStream.Close();
+            fileStream.Close();
+        }
+
+        public static byte[] Decrypt(byte[] cipherData, byte[] Key, byte[] IV)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            Rijndael rijndael = Rijndael.Create();
+            rijndael.Key = Key;
+            rijndael.IV = IV;
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateDecryptor(), CryptoStreamMode.Write);
+            cryptoStream.Write(cipherData, 0, cipherData.Length);
+            cryptoStream.Close();
+            return memoryStream.ToArray();
+        }
+
+        public static string Decrypt(string cipherText, string Password)
+        {
+            byte[] cipherData = Convert.FromBase64String(cipherText);
+            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(Password, new byte[]
+            {
+                73,
+                118,
+                97,
+                110,
+                32,
+                77,
+                101,
+                100,
+                118,
+                101,
+                100,
+                101,
+                118
+            });
+            byte[] bytes = Cryptography.Decrypt(cipherData, passwordDeriveBytes.GetBytes(32), passwordDeriveBytes.GetBytes(16));
+            return Encoding.Unicode.GetString(bytes);
+        }
+
+        public static byte[] Decrypt(byte[] cipherData, string Password)
+        {
+            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(Password, new byte[]
+            {
+                73,
+                118,
+                97,
+                110,
+                32,
+                77,
+                101,
+                100,
+                118,
+                101,
+                100,
+                101,
+                118
+            });
+            return Cryptography.Decrypt(cipherData, passwordDeriveBytes.GetBytes(32), passwordDeriveBytes.GetBytes(16));
+        }
+
+        public static void Decrypt(string fileIn, string fileOut, string Password)
+        {
+            FileStream fileStream = new FileStream(fileIn, FileMode.Open, FileAccess.Read);
+            FileStream stream = new FileStream(fileOut, FileMode.OpenOrCreate, FileAccess.Write);
+            PasswordDeriveBytes passwordDeriveBytes = new PasswordDeriveBytes(Password, new byte[]
+            {
+                73,
+                118,
+                97,
+                110,
+                32,
+                77,
+                101,
+                100,
+                118,
+                101,
+                100,
+                101,
+                118
+            });
+            Rijndael rijndael = Rijndael.Create();
+            rijndael.Key = passwordDeriveBytes.GetBytes(32);
+            rijndael.IV = passwordDeriveBytes.GetBytes(16);
+            CryptoStream cryptoStream = new CryptoStream(stream, rijndael.CreateDecryptor(), CryptoStreamMode.Write);
+            int num = 4096;
+            byte[] buffer = new byte[num];
+            int num2;
+            do
+            {
+                num2 = fileStream.Read(buffer, 0, num);
+                cryptoStream.Write(buffer, 0, num2);
+            }
+            while (num2 != 0);
+            cryptoStream.Close();
+            fileStream.Close();
+        }
+    }
+}
