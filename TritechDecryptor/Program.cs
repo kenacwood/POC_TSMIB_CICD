ebf4c553e9544f6cb6dcbe1eb66be5e49f8e3fd5
Ken Wood <ken.wood@ontario.ca>
2018-06-05 09:43:02 -0400

    PTC-1

--- TritechDecryptor/Program.cs
+++ TritechDecryptor/Program.cs
@@ -13,7 +13,7 @@ namespace TritechDecryptor
         /// </summary>
         [STAThread]
         static void Main()
-        {
+        { 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
